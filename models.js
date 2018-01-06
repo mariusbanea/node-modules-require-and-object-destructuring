325253a3c86440edde64ff3602684ed1e918d9d4
Glitch (code-todo-rest-api-base) <none>
2018-01-06 22:43:11 +0000

    🌱⛽️ Checkpoint models.js:163899/1

--- models.js
+++ models.js
@@ -1,1 +1,20 @@
-https://glitch.com/edit/#!/caramel-cappelletti?path=models.js:21:0
+const mongoose = require('mongoose');
+
+const todoSchema = mongoose.Schema({
+  title: String,
+  completed: {type: Boolean, default: false}
+});
+
+todoSchema.methods.serialize = function() {
+  return {
+    id: this._id,
+    title: this.title,
+    completed: this.completed
+  };
+};
+
+const Todo = mongoose.model("Todo", todoSchema);
+
+module.exports = Todo;
+
+
