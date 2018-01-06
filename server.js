7352ec20a4f45438c76fab95b4ee30c1a640737f
Glitch (alive-fridge) <none>
2018-01-06 03:47:37 +0000

    🚇👌 Checkpoint server.js:163899/526 .env:163899/12 models.js:163899/20 test/server.test.js:163899/36

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ mongoose.Promise = global.Promise;
 const Todo = require('./models');
 const { PORT, DATABASE_URL } = require('./config');
 
+const seedData = require('./db/todos.json');
+
 const app = express(); 
 
 app.use(express.json()); // Parse JSON body
@@ -14,94 +16,82 @@ app.use(express.json()); // Parse JSON body
  */
 app.get('/v1/todos', (req, res, next) => {
   Todo.find()
-    .then(list => res.json(list.serialize()))
+    // .then(list => res.json(list.serialize()))
+    .then(foo => res.json(foo))
     .catch(next); // error handler
-
 });
 
-app.post('/v1/todos', (req, res, next) => {
-  const { title } = req.body;
+// app.post('/v1/todos', (req, res, next) => {
+//   const { title } = req.body;
+//   /***** Never trust users - validate input *****/
+//   if (!title) {
+//     const err = new Error('Missing `title` in request body');
+//     err.status = 400;
+//     return next(err); // error handler
+//   }
+//   const newItem = { title };
+//   newItem.completed = false;
+//   // Using promises
+//   todos.create(newItem)
+//     .then(item => res.location(`/items/${item.id}`).status(201).json(item))
+//     .catch(next); // error handler
+// });
 
-  /***** Never trust users - validate input *****/
-  if (!title) {
-    const err = new Error('Missing `title` in request body');
-    err.status = 400;
-    return next(err); // error handler
-  }
-  const newItem = { title };
-  newItem.completed = false;
-  
-  // Using promises
-  todos.create(newItem)
-    .then(item => res.location(`/items/${item.id}`).status(201).json(item))
-    .catch(next); // error handler
+// app.get('/v1/todos/:id', (req, res, next) => {
+//   const id = req.params.id;
+//   // Using promises
+//   todos.findById(id)
+//     .then(item => {
+//       if (item) {
+//         res.json(item);
+//       } else {
+//         next(); // 404 handler
+//       }
+//     })
+//     .catch(next); // error handler
+// });
 
-});
+// app.put('/v1/todos/:id', (req, res, next) => {
+//   const id = req.params.id;
+//   /***** Never trust users - validate input *****/
+//   const replaceItem = {};
+//   const updateableFields = ['title', 'completed'];
+//   updateableFields.forEach(field => {
+//     if (field in req.body) {
+//       replaceItem[field] = req.body[field];
+//     }
+//   });
+//   /***** Never trust users - validate input *****/
+//   if (!replaceItem.title) {
+//     const err = new Error('Missing `title` in request body');
+//     err.status = 400;
+//     return next(err); // error handler
+//   }
+//   // Using promises
+//   app.findByIdAndUpdate(id, replaceItem)
+//     .then(item => {
+//       if (item) {
+//         res.json(item);
+//       } else {
+//         next(); // 404 handler
+//       }
+//     })
+//     .catch(next); // error handler
+// });
 
-app.get('/v1/todos/:id', (req, res, next) => {
-  const id = req.params.id;
-
-  // Using promises
-  todos.findById(id)
-    .then(item => {
-      if (item) {
-        res.json(item);
-      } else {
-        next(); // 404 handler
-      }
-    })
-    .catch(next); // error handler
-
-});
-
-app.put('/v1/todos/:id', (req, res, next) => {
-  const id = req.params.id;
-
-  /***** Never trust users - validate input *****/
-  const replaceItem = {};
-  const updateableFields = ['title', 'completed'];
-
-  updateableFields.forEach(field => {
-    if (field in req.body) {
-      replaceItem[field] = req.body[field];
-    }
-  });
-
-  /***** Never trust users - validate input *****/
-  if (!replaceItem.title) {
-    const err = new Error('Missing `title` in request body');
-    err.status = 400;
-    return next(err); // error handler
-  }
-
-  // Using promises
-  app.findByIdAndUpdate(id, replaceItem)
-    .then(item => {
-      if (item) {
-        res.json(item);
-      } else {
-        next(); // 404 handler
-      }
-    })
-    .catch(next); // error handler
-
-});
-
-app.delete('/v1/todos/:id', (req, res, next) => {
-  const id = req.params.id;
-
-  // Using promises
-  todos.findByIdAndRemove(id)
-    .then(count => {
-      if (count) {
-        res.status(204).end();
-      } else {
-        next(); // 404 handler
-      }
-    })
-    .catch(next); // error handler
-    
-});
+// app.delete('/v1/todos/:id', (req, res, next) => {
+//   const id = req.params.id;
+//   // Using promises
+//   todos.findByIdAndRemove(id)
+//     .then(count => {
+//       if (count) {
+//         res.status(204).end();
+//       } else {
+//         next(); // 404 handler
+//       }
+//     })
+//     .catch(next); // error handler
+// });
 
 // 404 catch-all
 app.use(function (req, res, next) {
@@ -121,6 +111,16 @@ app.use(function (err, req, res, next) {
 
 
 if (require.main === module) {
+  
+  mongoose.connect(DATABASE_URL, { useMongoClient: true })
+    .then(() => {
+      Todo.insertMany(seedData);  
+    })
+    .catch(err => {
+      console.error('ERROR: Mongoose failed to connect! Is the database running?');
+      console.error(err);
+    });  
+  
   const server = app.listen(PORT, function () {
     console.log('Your app is listening on port ' + server.address().port);
   });
