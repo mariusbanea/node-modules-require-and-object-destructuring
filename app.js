b191ac045c00986e470d97ba606aab95ffde0935
Glitch (rustic-aftershave) <none>
2018-01-06 23:49:58 +0000

    🐄🚏 Checkpoint app.js:163899/725 modules/calculator.js:163899/4 README.md:163899/7

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
-
+/*************
+ * require the calculator module and unpack the properties using ES6 object destructuring
+ *************/
 
 const {add, subtract, multiply, divide} = require('./modules/calculator');
 
-/******
- *
- * DO NOT CHANGE CODE BELOW THIS POINT
- *
- *****/
 
-var _0x5277=["\x65\x78\x70\x6F\x72\x74\x73"];const app={add:add,subtract:subtract,multiply:multiple,divide:divide};module[_0x5277[0]]= app
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/************** DO NOT EDIT BELOW THIS LINE *************/
+eval(function(p,a,c,k,e,d){e=function(c){return c};if(!''.replace(/^/,String)){while(c--){d[c]=k[c]||c}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('1.0={2,3,4,5};',6,6,'exports|module|add|subtract|multiply|divide'.split('|'),0,{}))
