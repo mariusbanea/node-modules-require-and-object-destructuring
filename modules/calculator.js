dd8074919bc4580039c4b2b57c4736d8d4f5f398
Glitch (horn-space) <none>
2018-01-31 16:10:46 +0000

    🛬🎅 Checkpoint modules/calculator.js:366375/1

--- modules/calculator.js
+++ modules/calculator.js
@@ -1,16 +1,16 @@
 const action = {
   add: function add(a, b) {
-  return a + b
-},
-  subtract: function subtract(a, b) {
-  return a - b
-},
-  multiply: function multiply(a, b) {
-  return a * b;
-},
-  divide: function divide(a, b) {
-  return a / b;
-}
+    return a + b
+  },
+    subtract: function subtract(a, b) {
+    return a - b
+  },
+    multiply: function multiply(a, b) {
+    return a * b;
+  },
+    divide: function divide(a, b) {
+    return a / b;
+  }
 }
 
 
